e118d27b0c404e46e0f2f7700e0b9258230bbd8b
hichamhabtoune21 <99260356+hichamhabtoune21@users.noreply.github.com>
2023-01-30 15:04:01 +0100

    Add files via upload

--- FRONT END/dashboard/admin-dashboard.php
+++ FRONT END/dashboard/admin-dashboard.php
@@ -1,6 +1,9 @@
 <?php
 session_start();
 ?>
+<!doctype html>
+<html>
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -15,29 +18,79 @@ session_start();
         body {
             font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
         }
-        i{
+
+        i {
             padding-right: 10px;
         }
+
+        @media(prefers-reduced-motion: reduce) {
+            .collapsing {
+                transition-property: height, visibility;
+                transition-duration: .999s;
+            }
+        }
+
+        @media (max-width: 450px) {
+            .h-custom {
+                height: 100%;
+            }
+        }
     </style>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"
+        integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3"
+        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js"
+        integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V"
+        crossorigin="anonymous"></script>
+
 </head>
 
 <body>
 
-
     <div class="container-fluid">
-        <div class="row row-space" style="height: 100%;">
-            <div class="col-md-2 col-lg-2 col-xl-2 bg-dark float-left" style="color: beige; border-top-right-radius: 3%;">
 
-                <h3 style="padding: 20px;">ACL</h3>
-                <nav class="navbar navbar-dark bg-dark">
-                    <div id="navbarText" style="font-size: 20px; padding-left: 20px;">
-                        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <div class="row row-space vw-100">
+            <div class="col-md-2 col-lg-2 col-xl-2 bg-dark float-left" style="color: beige;">
+                <div class="row d-md-none">
+
+                    <div class="col-6 inline d-flex justify-content-between">
+
+
+                        <nav class="navbar navbar-dark">
+                            <button class="navbar-toggler" data-bs-toggle="collapse" href="#nav" role="button"
+                                aria-expanded="true" aria-controls="nav" data-toggle="collapse">
+                                <i class="navbar-toggler-icon"></i>
+                            </button>
+                            <span class="h2" style="padding-left: 20px; padding-top: 5px;">Menu</span>
+
+                        </nav>
+
+                    </div>
+
+                    <div class="col-6">
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <div class="row">
+        <div class="col-md-2 col-lg-2 col-xl-2 bg-dark collapse show d-md-block text-nowrap" 
+            style="color: beige; border-radius: 5%; padding-right: 30px;" id="nav">
+            <div class="vh-100">
+                <div class="navbar navbar-dark bg-dark">
+                    <div style="font-size: 20px; padding-left: 20px; padding-top: 20px;">
+                        <ul class="navbar-nav">
                             <li class="nav-item" style="padding-bottom: 20px;">
-                                <a class="nav-link active" aria-current="page" href="#"><i class="bi bi-house"></i>Home</a>
+                                <a class="nav-link active" aria-current="page" href="#"><i
+                                        class="bi bi-house"></i>Home</a>
                             </li>
 
                             <li class="nav-item" style="padding-bottom: 20px;">
-                                <a class="nav-link" aria-current="page" href="../../BACK END/all_users.php"><i class="bi bi-people"></i>Users</a>
+                                <a class="nav-link" aria-current="page" href="../../BACK END/all_users.php"><i
+                                        class="bi bi-people"></i>Users</a>
                             </li>
 
                             <li class="nav-item" style="padding-bottom: 20px;">
@@ -46,25 +99,67 @@ session_start();
                             <li class="nav-item" style="padding-bottom: 20px;">
                                 <a class="nav-link" href="#"><i class="bi bi-bar-chart"></i>Overview</a>
                             </li>
-                            
+
+                            <li class="nav-item" style="padding-bottom: 20px;">
+                                <a class="nav-link" href="#"><i class="bi bi-question-square"></i>Help</a>
+                            </li>
                             <li class="nav-item" style="padding-bottom: 20px;">
                                 <a class="nav-link" href="#"><i class="bi bi-question-square"></i>Help</a>
                             </li>
                         </ul>
                     </div>
-                </nav>
+                </div>
             </div>
-            
-            <div class="col-md-10 col-lg-10 col-xl-10" style="background-color: white;">
-                <h1 style="font-size: 50px; padding: 20px;">
-                   <?php
-                   echo "Welcome " . $_SESSION["username"]; 
-                   ?>
-                
-                </h1>
 
 
-            </div>
+        </div>
+
+        <div class="col-md-10 col-lg-10 col-xl-10" style="background-color: white;">
+            <h1 style="font-size: 50px; padding-left: 20px;">
+                Welcome <?php echo $_SESSION["Username"];?>
+
+            </h1>
+
+
         </div>
     </div>
+    <div class="o" id="boh" style="display: none;">
+        brutto
+    </div>
 </body>
+<script>
+    /* Storing user's device details in a variable*/
+    let details = navigator.userAgent;
+
+    /* Creating a regular expression 
+    containing some mobile devices keywords 
+    to search it in details string*/
+    let regexp = /android|iphone|kindle|ipad/i;
+
+    /* Using test() method to search regexp in details
+    it returns boolean value*/
+    let isMobileDevice = regexp.test(details);
+
+    const a = ["desktop", "mobile"];
+    if (isMobileDevice) {
+        //document.write("You are using a Mobile Device");
+        //var x = document.getElementById("select").value;
+
+
+
+    } else {
+        // document.write("You are using Desktop");
+        // document.getElementById("nav").classList.add('collapse-horizontal');
+
+    }
+    function change() {
+
+
+    }
+
+
+
+
+</script>
+
+</html>
