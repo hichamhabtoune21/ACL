f37a22b25739892c013b825c0e06988ee2910676
hichamhabtoune21 <hicham.habtoune@ptpvenezia.edu.it>
2023-03-20 18:26:20 +0100

    #

--- DATABASE/crud.sql
+++ DATABASE/crud.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Creato il: Feb 13, 2023 alle 21:50
--- Versione del server: 10.4.27-MariaDB
--- Versione PHP: 8.2.0
+-- Generation Time: Mar 20, 2023 at 06:21 PM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.2.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,7 +24,7 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Struttura della tabella `client`
+-- Table structure for table `client`
 --
 
 CREATE TABLE `client` (
@@ -37,7 +37,7 @@ CREATE TABLE `client` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dump dei dati per la tabella `client`
+-- Dumping data for table `client`
 --
 
 INSERT INTO `client` (`ID_Client`, `VAT number`, `Name`, `Surname`, `Phone`, `Address`) VALUES
@@ -47,7 +47,7 @@ INSERT INTO `client` (`ID_Client`, `VAT number`, `Name`, `Surname`, `Phone`, `Ad
 -- --------------------------------------------------------
 
 --
--- Struttura della tabella `invoice`
+-- Table structure for table `invoice`
 --
 
 CREATE TABLE `invoice` (
@@ -61,17 +61,18 @@ CREATE TABLE `invoice` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dump dei dati per la tabella `invoice`
+-- Dumping data for table `invoice`
 --
 
 INSERT INTO `invoice` (`ID_Invoice`, `Progressive number`, `Issuing date`, `Business name`, `Amount`, `Payment type`, `ID_Client`) VALUES
-(30, 10, '2023-02-19', 'bellissimo', 10, 'card', 1),
-(36, 1111, '2023-03-04', 'bellissimo', 9, 'cash', 1);
+(30, 10, '2023-02-19', 'aaaaa', 1, 'card', 1),
+(36, 1111, '2023-03-04', 'siiiiiiiiiiiiiii', 9, 'cash', 1),
+(42, 99, '2023-02-24', 'a', 10, 'card', 2);
 
 -- --------------------------------------------------------
 
 --
--- Struttura della tabella `permission`
+-- Table structure for table `permission`
 --
 
 CREATE TABLE `permission` (
@@ -81,7 +82,7 @@ CREATE TABLE `permission` (
 -- --------------------------------------------------------
 
 --
--- Struttura della tabella `permission_belong_to_role`
+-- Table structure for table `permission_belong_to_role`
 --
 
 CREATE TABLE `permission_belong_to_role` (
@@ -92,7 +93,7 @@ CREATE TABLE `permission_belong_to_role` (
 -- --------------------------------------------------------
 
 --
--- Struttura della tabella `role`
+-- Table structure for table `role`
 --
 
 CREATE TABLE `role` (
@@ -100,7 +101,7 @@ CREATE TABLE `role` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dump dei dati per la tabella `role`
+-- Dumping data for table `role`
 --
 
 INSERT INTO `role` (`Role`) VALUES
@@ -112,7 +113,7 @@ INSERT INTO `role` (`Role`) VALUES
 -- --------------------------------------------------------
 
 --
--- Struttura della tabella `user`
+-- Table structure for table `user`
 --
 
 CREATE TABLE `user` (
@@ -126,7 +127,7 @@ CREATE TABLE `user` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dump dei dati per la tabella `user`
+-- Dumping data for table `user`
 --
 
 INSERT INTO `user` (`ID_User`, `Email`, `Password`, `Username`, `Name`, `Surname`, `Role`) VALUES
@@ -136,7 +137,7 @@ INSERT INTO `user` (`ID_User`, `Email`, `Password`, `Username`, `Name`, `Surname
 -- --------------------------------------------------------
 
 --
--- Struttura della tabella `user_manage_invoice`
+-- Table structure for table `user_manage_invoice`
 --
 
 CREATE TABLE `user_manage_invoice` (
@@ -145,102 +146,102 @@ CREATE TABLE `user_manage_invoice` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Indici per le tabelle scaricate
+-- Indexes for dumped tables
 --
 
 --
--- Indici per le tabelle `client`
+-- Indexes for table `client`
 --
 ALTER TABLE `client`
   ADD PRIMARY KEY (`ID_Client`);
 
 --
--- Indici per le tabelle `invoice`
+-- Indexes for table `invoice`
 --
 ALTER TABLE `invoice`
   ADD PRIMARY KEY (`ID_Invoice`),
   ADD KEY `ID_Cliente(FK)` (`ID_Client`);
 
 --
--- Indici per le tabelle `permission`
+-- Indexes for table `permission`
 --
 ALTER TABLE `permission`
   ADD PRIMARY KEY (`Permission`);
 
 --
--- Indici per le tabelle `permission_belong_to_role`
+-- Indexes for table `permission_belong_to_role`
 --
 ALTER TABLE `permission_belong_to_role`
   ADD KEY `Permesso(FK)` (`Permission`),
   ADD KEY `Ruolo_FK` (`Role`);
 
 --
--- Indici per le tabelle `role`
+-- Indexes for table `role`
 --
 ALTER TABLE `role`
   ADD PRIMARY KEY (`Role`);
 
 --
--- Indici per le tabelle `user`
+-- Indexes for table `user`
 --
 ALTER TABLE `user`
   ADD PRIMARY KEY (`ID_User`),
   ADD KEY `Ruolo(FK)` (`Role`);
 
 --
--- Indici per le tabelle `user_manage_invoice`
+-- Indexes for table `user_manage_invoice`
 --
 ALTER TABLE `user_manage_invoice`
   ADD KEY `ID_Fattura(FK)` (`ID_Invoice`),
   ADD KEY `ID_User(FK)` (`ID_User`);
 
 --
--- AUTO_INCREMENT per le tabelle scaricate
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT per la tabella `client`
+-- AUTO_INCREMENT for table `client`
 --
 ALTER TABLE `client`
   MODIFY `ID_Client` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
--- AUTO_INCREMENT per la tabella `invoice`
+-- AUTO_INCREMENT for table `invoice`
 --
 ALTER TABLE `invoice`
-  MODIFY `ID_Invoice` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=42;
+  MODIFY `ID_Invoice` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=43;
 
 --
--- AUTO_INCREMENT per la tabella `user`
+-- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
   MODIFY `ID_User` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
--- Limiti per le tabelle scaricate
+-- Constraints for dumped tables
 --
 
 --
--- Limiti per la tabella `invoice`
+-- Constraints for table `invoice`
 --
 ALTER TABLE `invoice`
   ADD CONSTRAINT `ID_Cliente(FK)` FOREIGN KEY (`ID_Client`) REFERENCES `client` (`ID_Client`);
 
 --
--- Limiti per la tabella `permission_belong_to_role`
+-- Constraints for table `permission_belong_to_role`
 --
 ALTER TABLE `permission_belong_to_role`
   ADD CONSTRAINT `Permesso(FK)` FOREIGN KEY (`Permission`) REFERENCES `permission` (`Permission`),
   ADD CONSTRAINT `Ruolo_FK` FOREIGN KEY (`Role`) REFERENCES `role` (`Role`);
 
 --
--- Limiti per la tabella `user`
+-- Constraints for table `user`
 --
 ALTER TABLE `user`
   ADD CONSTRAINT `Ruolo(FK)` FOREIGN KEY (`Role`) REFERENCES `role` (`Role`);
 
 --
--- Limiti per la tabella `user_manage_invoice`
+-- Constraints for table `user_manage_invoice`
 --
 ALTER TABLE `user_manage_invoice`
   ADD CONSTRAINT `ID_Fattura(FK)` FOREIGN KEY (`ID_Invoice`) REFERENCES `invoice` (`ID_Invoice`),
