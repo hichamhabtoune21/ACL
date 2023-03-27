aace91bfbcee73a5e6401ef33a9d63c805cd8ebe
hichamhabtoune21 <hicham.habtoune@ptpvenezia.edu.it>
2023-03-27 20:37:49 +0200

    #

--- BACK END/connect.php
+++ BACK END/connect.php
@@ -1,5 +1,6 @@
 <?php
-$connect = new mysqli("db", "admin","admin","crud");
+$connect = new mysqli("db", "admin","admin","crud"); //con docker
+//$connect = new mysqli("localhost", "root","","crud"); //con xampp
 if(!$connect){
     echo "Connection server error";
 }
