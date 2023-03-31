9b1726589ddbd821cd55028e903cec240bbf3155
hichamhabtoune21 <hicham.habtoune@ptpvenezia.edu.it>
2023-03-31 19:39:29 +0200

    #

--- BACK END/script_signup.php
+++ BACK END/script_signup.php
@@ -15,21 +15,24 @@ mysqli_query($connect, "SET FOREIGN_KEY_CHECKS=0");
 if ($password != $password1) {
     $t["text"] = $t["text"] . "The passwords entered are different\n";
 }
+$query= "SELECT Email FROM user where Email = ?";
+$check_email=$connect->prepare($query);
+$check_email->bind_param("s",$email);
+$check_email->execute();
 
-$check_email = mysqli_query($connect, "SELECT Email FROM user where Email = '$email' "); //cerca se ci sono email uguali a quelle già messe
-
-if (mysqli_num_rows($check_email) > 0) { // se esiste già un'email uguale
+if (mysqli_num_rows($check_email->get_result()) > 0) { // se esiste già un'email uguale
     $t["text"] = $t["text"] . "The email entered already exists\n";
 } 
 else {
      //altrimenti inserisco i dati nel database
-    $query = "INSERT INTO `user` (`Email`, `Password`, `Username`, `Name`, `Surname`,`Role`) VALUES ('$email', '$hashed_password', '$username','$name','$surname','NULL')";
-    $result = mysqli_query($connect, $query);
-    
-    if ($result) {
+    $query = "INSERT INTO `user` (`Email`, `Password`, `Username`, `Name`, `Surname`) VALUES (?,?,?,?,?)";
+    $result=$connect->prepare($query);
+    $result->bind_param("sssss",$email,$hashed_password,$username,$name,$surname);
+    $result->execute();    
+    if ($result->get_result() || mysqli_affected_rows($connect) > 0) {
         $t["text"] = "Thanks! You are now registered\n";
     } else {
-        $t["text"] = "Error inserting data into the database\n";
+        $t["text"] = "Error inserting data into the database: " . mysqli_error($connect) . "\n";
     }
 }
 echo json_encode(array("text" => $t["text"]));
