808663e5eef69aa765612304186e347d3f5c6458
hichamhabtoune21 <99260356+hichamhabtoune21@users.noreply.github.com>
2023-05-11 11:20:26 +0200

    #

--- Dockerfile
+++ Dockerfile
@@ -2,3 +2,23 @@ FROM php:8.2-apache
 RUN apt-get update && apt-get upgrade -y
 RUN docker-php-ext-install mysqli && docker-php-ext-enable mysqli
 RUN docker-php-ext-install gettext && docker-php-ext-enable gettext
+
+RUN apk update \
+    && apk add --no-cache git \
+    && curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
+
+COPY composer.json composer.lock /app/
+RUN composer install --no-scripts --no-autoloader --no-dev --prefer-dist
+
+
+RUN php composer.phar dump-autoload --no-scripts --no-dev --optimize
+
+RUN php bin/console cache:warmup
+
+RUN docker-php-ext-configure intl \
+    && docker-php-ext-install intl \
+    
+COPY translations.yaml config/packages/translations.yaml
+COPY services.yaml config/services.yaml
+
+CMD ["php", "bin/console", "translation:extract"]
--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+/*
 // Imposta la lingua predefinita per la tua applicazione
 $default_language = "it_IT";
 
@@ -13,4 +14,36 @@ putenv("LC_ALL=$default_language");
 setlocale(LC_ALL, $default_language);
 bindtextdomain($domain, $locale_directory);
 textdomain($domain);
+echo _("Signup");
+phpinfo();
+*/
+use Davibennun\LaravelTranslationManager\Manager;
+use Symfony\Component\Translation\Loader\PhpFileLoader;
+use Symfony\Component\Translation\Loader\YamlFileLoader;
+use Symfony\Component\Translation\Translator;
+use Symfony\Component\Config\FileLocator;
+
+// Configura la cartella "lang"
+$langPath = __DIR__ . '/lang';
+
+// Crea un'istanza del gestore delle traduzioni
+$manager = new Manager();
+
+// Carica le traduzioni per la lingua di default (solitamente inglese)
+$translator = new Translator('en');
+$translator->addLoader('php', new PhpFileLoader());
+$translator->addResource('php', $langPath.'/en.php', 'en');
+$manager->addTranslations('en', $translator);
+
+// Aggiungi una nuova traduzione o aggiorna una traduzione esistente
+$manager->setTranslation('en', 'key', 'value');
+
+// Salva le modifiche apportate ai file di traduzione
+$locator = new FileLocator($langPath);
+$loader = new PhpFileLoader();
+foreach ($manager->translations('en') as $domain => $translations) {
+    $messages = $translator->getCatalogue('en')->all($domain);
+    $loader->dump($messages, ['path' => $langPath, 'resource' => $domain.'.php']);
+}
+
 ?>
