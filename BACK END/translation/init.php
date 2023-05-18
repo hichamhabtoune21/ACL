20c2510b9c879afc0320a9d91b4f44c72e8b4ac2
hichamhabtoune21 <hicham.habtoune@ptpvenezia.edu.it>
2023-05-18 11:04:41 +0200

    traduzione

--- BACK END/translation/init.php
+++ BACK END/translation/init.php
@@ -16,12 +16,17 @@ $translator->addLoader('array', new ArrayLoader());
 // Aggiungi le traduzioni per la lingua italiana
 require 'locale/en_US.php';
 require 'locale/it_IT.php';
+require 'locale/es_ES.php';
+
 
 $translator->addResource('array', $itTranslations, 'it_IT');
 
 // Aggiungi le traduzioni per la lingua inglese
 
 $translator->addResource('array', $enTranslations, 'en_US');
+
+$translator->addResource('array', $esTranslations, 'es_ES');
+
 // Imposta la lingua corrente dell'utente
 
 ?>
